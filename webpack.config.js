8f38c7f7e454ad2fd00ffecb9d4094bfa7478540
Benjamin Zuill-Smith <bzuillsmith@gmail.com>
2024-01-20 16:51:33 -0800

    Most basic react app with JSX support

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+  };
